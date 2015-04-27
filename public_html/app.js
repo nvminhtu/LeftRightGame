4cfd2706802d8013280cd088c247cb73fe23ce42
Nicholas Dupuis <nicholasdupuis@ge.com>
2015-04-27 15:35:08 -0500

    Initial commit

---
+++ public_html/app.js
@@ -0,0 +1,30 @@
+(function () {
+    var app = angular.module('leftRightGame', []);
+
+    app.controller('GameController', function ($scope, $interval) {
+        $scope.gameState = {
+            timeRemaining: 30,
+            score: 0,
+            direction: null
+        };
+
+        //Start Game Timer
+        $scope.startGame = function () {
+           $scope.timer = $interval(function () {
+                $scope.gameState.timeRemaining--;
+                console.log($scope.gameState.timeRemaining);
+            }, 1000, 30);
+        }
+        
+        //Reset the game
+        $scope.resetGame = function () {
+            $interval.cancel($scope.timer);
+            $scope.gameState.timeRemaining = 30;
+            $scope.gameState.direction = null;
+        }
+
+    });
+
+
+
+})();
